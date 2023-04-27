e4c65b9d9c322675660822a47663ce116d97c4f2
Tristan Perry <tristan@aheadofthecurve.co.za>
2023-04-27 09:11:19 +0200

    save method for general dao

--- src/main/java/dao/TTDAO.java
+++ src/main/java/dao/TTDAO.java
@@ -1,6 +1,8 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -61,4 +63,55 @@ public class TTDAO<T extends clientObject> {
         }
         return entities;
     }
+    
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/trucktasker";
+    private static final String DATABASE_USERNAME = "root";
+    private static final String DATABASE_PASSWORD = "adminadmin";
+    
+    public static boolean saveData(String tableName, String[] columnNames, Object[] values) {
+        boolean success = false;
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+            StringBuilder sql = new StringBuilder("INSERT INTO " + tableName + " (");
+            for (int i = 0; i < columnNames.length; i++) {
+                sql.append(columnNames[i]);
+                if (i < columnNames.length - 1) {
+                    sql.append(", ");
+                }
+            }
+            sql.append(") VALUES (");
+            for (int i = 0; i < values.length; i++) {
+                sql.append("?");
+                if (i < values.length - 1) {
+                    sql.append(", ");
+                }
+            }
+            sql.append(")");
+            statement = connection.prepareStatement(sql.toString());
+            for (int i = 0; i < values.length; i++) {
+                statement.setObject(i + 1, values[i]);
+            }
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected > 0) {
+                success = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return success;
+    }
 }
+
